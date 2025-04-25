4f1567b8122a13bde18e502e05388d9246822bb9
EsaRamadhanFaruq <esa.faruq26@gmail.com>
2025-04-25 20:05:18 +0700

    Add files via upload

---
+++ esa2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout<<"Masukan kata = HALO"<<endl;
+	cin>>kata;
+	
+	if (kata=="HALO"){
+		cout<<"Kata yang di masukan sesuai"<<endl;
+	}else{
+		cout<<"Kata yang di masukan tidak sesuai"<<endl;
+	}
+}
